cb6ec359452b5cf34ff27d26448a923d83c35775
Reza Chairul <98737769+rezachairul@users.noreply.github.com>
2025-08-24 15:57:51 +0700

    delete unused Seeder

--- database/seeders/KelolaBeritaSeeder.php
+++ database/seeders/KelolaBeritaSeeder.php
@@ -11,14 +11,9 @@ class KelolaBeritaSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run()
+    public function run()  
     {
-        // Mix berbagai jenis berita
-        KelolaBerita::factory()->published()->count(15)->create();
-        KelolaBerita::factory()->draft()->count(5)->create();
-        KelolaBerita::factory()->academic()->published()->count(8)->create();
-        KelolaBerita::factory()->library()->published()->count(7)->create();
-        KelolaBerita::factory()->recent()->count(10)->create();
+        //
     }
 
 }
